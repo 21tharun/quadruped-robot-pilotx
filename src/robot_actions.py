e4531b9a5d4e61fb1e224204f122a5c75bf2cec1
Tharun Vasudev <112506088+21tharun@users.noreply.github.com>
2025-12-23 20:33:39 +0530

    Implement move_servo function in robot_actions.py
    Adds a function to move a servo to a specified angle.

---
+++ src/robot_actions.py
@@ -0,0 +1,7 @@
+# Low-level hardware abstraction layer (PilotX firmware)
+def move_servo(servo_id, angle):
+    """
+    Moves the specified servo to the given angle.
+    Servo control is handled by PilotX firmware.
+    """
+    pass
